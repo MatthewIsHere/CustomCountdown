a48a0182cae5bbb324fa6d12f87025b7c7b92594
Matthew Lyon <matthew@Matthewlyon.net>
2020-05-27 22:09:45 -0400

    fixed for final version with corrected negative time in countdown. docs coming next

--- script.js
+++ script.js
@@ -3,29 +3,57 @@ function pad(n, width, z) {
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
-function diff_hours(dt2, dt1) {
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000
-  var obj = {
-    minutes: Math.floor(diff / 60),
-    seconds: Math.floor(diff % 60)
+function getCountdown(target, other) {
+  var diff = (target.getTime() - other.getTime()) / 1000
+  let seconds = Math.round(Math.abs(diff))
+  let sign
+  switch(Math.sign(diff)) {
+    case -1:
+    sign = "+"
+    break
+    case 1:
+    sign = "-"
+    break
+    case 0:
+    sign = ""
+    break
+    default:
+    sign = ""
   }
+  var obj = {
+    absSeconds: seconds,
+    sign : sign,
+    hours: pad(Math.floor(seconds/60/60), 2),
+    minutes: pad(Math.floor(seconds / 60)%60, 2),
+    seconds: pad(Math.floor(seconds % 60), 2)
+  }
+  console.log(obj)
   return obj
 }
 async function countdownTimer() {
   let res = await fetch("settings.json")
   let json = await res.json()
-  document.querySelector("title").innerHTML = json.title
-  document.querySelector("#container").style.backgroundColor = json.backgroundColor
-  document.querySelector("#countdownContainer").style.color = json.textColor
-  document.querySelector("#countdown").style.fontSize = json.timeFontSize
-  document.querySelector("#title").style.fontSize = ("mainTitleSize" in json) ? json.mainTitleSize : ""
-  document.querySelector("#title").innerHTML = (json.mainTitle == false) ? "" : json.mainTitle
+  let target = new Date(json.time)
+  if ("displaySettings" in json) {
+    let displaySettings = json.displaySettings
+    if (displaySettings.window) {
+      let window = json.displaySettings.window
+      if (window.title) document.querySelector("title").innerHTML = window.title
+      if (window.backgroundColor) document.querySelector("#container").style.backgroundColor = window.backgroundColor
+      if (window.textColor) document.querySelector("#container").style.color = window.textColor
+      if (window.fontSize) document.querySelector("#countdown").style.fontSize = window.fontSize
+    }
+    if (displaySettings.mainTitle) {
+      let mainTitle = displaySettings.mainTitle
+      if (mainTitle.text) document.querySelector("#title").innerHTML = mainTitle.text
+      if (mainTitle.textSize) document.querySelector("#title").style.fontSize = mainTitle.textSize
+      if (mainTitle.textColor) document.querySelector("#title").style.color = mainTitle.textColor
+    }
+  }
   setInterval(() => {
-    let target = new Date(json.time)
     let current = new Date()
-    let obj = diff_hours(target, current)
-    document.querySelector("#countdown").innerHTML = `${obj.minutes}:${pad(obj.seconds, 2)}`
+    let time = getCountdown(target, current)
+    document.querySelector("#countdown").innerHTML = `${(json.includeT) ? "T": ""}${(json.includeSign) ? time.sign : ""}${time.hours}:${time.minutes}:${time.seconds}`
   }, 500)
-
 }
 countdownTimer()
